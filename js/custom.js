db805fd9d31401d804a414ac5493d065de63e915
miirbat <mirna.plakalovic@gmail.com>
2019-03-29 23:28:52 -0400

    beautify files

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
-$(function(){
-  $(".navbar-collapse ul li a").on("click touch", function(){
+$(function() {
+  $(".navbar-collapse ul li a").on("click touch", function() {
     $(".navbar-toggle").click();
   });
 });
 
-$(function () {
-  $(document).scroll(function () {
+$(function() {
+  $(document).scroll(function() {
     var $nav = $(".navbar-fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
